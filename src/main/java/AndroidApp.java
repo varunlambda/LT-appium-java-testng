ccde48a9ed097429623c46ecaf31949a901546f4
varunlambda <108345053+varunlambda@users.noreply.github.com>
2024-01-18 16:50:58 +0530

    Update AndroidApp.java

--- src/main/java/AndroidApp.java
+++ src/main/java/AndroidApp.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
-import os;
 
 import java.net.URL;
 import java.util.List;
