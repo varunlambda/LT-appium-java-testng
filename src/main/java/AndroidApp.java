1ce11c107e941e866285ef6d4c20fcef3cd72ade
varunlambda <108345053+varunlambda@users.noreply.github.com>
2024-09-04 18:12:00 +0530

    Update AndroidApp.java

--- src/main/java/AndroidApp.java
+++ src/main/java/AndroidApp.java
@@ -34,7 +34,7 @@ public class AndroidApp {
             capabilities.setCapability("platformName", platform);
             capabilities.setCapability("isRealMobile", true);
             //AppURL (Create from Wikipedia.apk sample in project)
-            capabilities.setCapability("app", "lt://APP1016029691704450197909703"); //Enter your app url
+            // capabilities.setCapability("app", "lt://APP1016029691704450197909703"); //Enter your app url
             capabilities.setCapability("deviceOrientation", "PORTRAIT");
             capabilities.setCapability("console", true);
             capabilities.setCapability("network", false);
